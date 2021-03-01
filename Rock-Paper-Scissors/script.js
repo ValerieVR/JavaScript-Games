316c663008a81b0c16d3baed676ae1c81b7ff320
Valerie <akpotevalerie@gmail.com>
2021-03-01 16:33:42 +0100

    Add DOM variables

---
+++ Rock-Paper-Scissors/script.js
@@ -0,0 +1,12 @@
+let playerHand = document.getElementById("player-hand");
+let opponentHand = document.getElementById("opponent-hand");
+
+let playerScore = document.getElementById("player-score");
+let opponentScore = document.getElementById("opponent-score");
+
+let wonOrLostMsg = document.getElementById("wonOrLost-msg");
+let resultExplanation = document.getElementById("result-explanation");
+let roundIndication = document.getElementById("document-indication");
+
+let playerChoices = document.querySelectorAll(".player-choice");
+let resetBttn = document.getElementById("reset");
